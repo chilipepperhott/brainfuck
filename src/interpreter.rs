640458deeba675180162c083cca1f58e4e8b999e
Elijah Potter <elijah.sirius@protonmail.com>
2023-07-20 21:04:01 -0600

    Added CLI parser generator

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 
 use crate::{op::Op, parser::Program};
 
@@ -22,7 +22,11 @@ impl Interpreter {
 
     /// Bool returns whether the program is now complete. True = complete.
     /// Result returns any IO error that occurs.
-    pub fn step(&mut self, out: &mut impl Write) -> std::io::Result<bool> {
+    pub fn step(
+        &mut self,
+        input: &mut impl Read,
+        output: &mut impl Write,
+    ) -> std::io::Result<bool> {
         let cur_op = self.program[self.pc];
         let cur_m = &mut self.memory[self.mp];
 
@@ -31,8 +35,12 @@ impl Interpreter {
             Op::Right => self.mp += 1,
             Op::Inc => *cur_m = (*cur_m).overflowing_add(1).0,
             Op::Dec => *cur_m = (*cur_m).overflowing_sub(1).0,
-            Op::Put => out.write_all(&[*cur_m])?,
-            Op::Get => todo!(),
+            Op::Put => output.write_all(&[*cur_m])?,
+            Op::Get => {
+                let mut buf = [0; 1];
+                input.read_exact(&mut buf)?;
+                *cur_m = buf[0];
+            }
             Op::Open { jump_to } => {
                 if *cur_m == 0 {
                     self.pc = jump_to;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::io;
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+use structopt::StructOpt;
 
 use crate::{interpreter::Interpreter, parser::Parser, tokens::Token};
 
@@ -7,62 +12,30 @@ mod op;
 mod parser;
 mod tokens;
 
-fn main() {
-    let bf = r#"
-        [ This program prints "Hello World!" and a newline to the screen, its
-          length is 106 active command characters. [It is not the shortest.]
-        
-          This loop is an "initial comment loop", a simple way of adding a comment
-          to a BF program such that you don't have to worry about any command
-          characters. Any ".", ",", "+", "-", "<" and ">" characters are simply
-          ignored, the "[" and "]" characters just have to be balanced. This
-          loop and the commands it contains are ignored because the current cell
-          defaults to a value of 0; the 0 value causes this loop to be skipped.
-        ]
-        ++++++++               Set Cell #0 to 8
-        [
-            >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
-            [                   as the cell will be cleared by the loop
-                >++             Add 2 to Cell #2
-                >+++            Add 3 to Cell #3
-                >+++            Add 3 to Cell #4
-                >+              Add 1 to Cell #5
-                <<<<-           Decrement the loop counter in Cell #1
-            ]                   Loop until Cell #1 is zero; number of iterations is 4
-            >+                  Add 1 to Cell #2
-            >+                  Add 1 to Cell #3
-            >-                  Subtract 1 from Cell #4
-            >>+                 Add 1 to Cell #6
-            [<]                 Move back to the first zero cell you find; this will
-                                be Cell #1 which was cleared by the previous loop
-            <-                  Decrement the loop Counter in Cell #0
-        ]                       Loop until Cell #0 is zero; number of iterations is 8
-        
-        The result of this is:
-        Cell no :   0   1   2   3   4   5   6
-        Contents:   0   0  72 104  88  32   8
-        Pointer :   ^
-        
-        >>.                     Cell #2 has value 72 which is 'H'
-        >---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
-        +++++++..+++.           Likewise for 'llo' from Cell #3
-        >>.                     Cell #5 is 32 for the space
-        <-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
-        <.                      Cell #3 was set to 'o' from the end of 'Hello'
-        +++.------.--------.    Cell #3 for 'rl' and 'd'
-        >>+.                    Add 1 to Cell #5 gives us an exclamation point
-        >++.                    And finally a newline from Cell #6
-        "#;
+#[derive(Debug, StructOpt)]
+#[structopt(name = "brainfuck", about = "A Brainfuck Interpreter.")]
+struct Opts {
+    pub file: String,
+}
 
-    let tokens = Token::lex_from_iter(bf.chars());
+fn main() {
+    let opts = Opts::from_args();
+
+    let mut file = File::open(opts.file).expect("Could not open file.");
+    let mut program = String::new();
+    file.read_to_string(&mut program)
+        .expect("Could not read from file.");
+
+    let tokens = Token::lex_from_iter(program.chars());
     let program = Parser::parse_full(tokens).unwrap();
 
     let mut interpreter = Interpreter::new(program);
 
     let mut stdout = io::stdout();
+    let mut stdin = io::stdin();
 
     loop {
-        if interpreter.step(&mut stdout).unwrap() {
+        if interpreter.step(&mut stdin, &mut stdout).unwrap() {
             break;
         }
     }
